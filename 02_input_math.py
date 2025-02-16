153a440fd22f3b57d66561696d4441f8a2c90602
AnnieC123 <anniechen@rhs.school.nz>
2025-02-17 10:45:10 +1300

    Added Multiplication

--- 02_input_math.py
+++ 02_input_math.py
@@ -28,12 +28,14 @@ name = input("What is your name? ")
 num_1 = int(input("What is your favourite number? "))
 num_2 = int(input("What is your second favourite number?"))
 
-# add numbers together
+# add numbers together3
 add = num_1 + num_2
 
 # multiply numbers together
+multiply = num_1 * num_2
 
 # greet user and display math
 print("Hello", name)
 
 print("{} + {} = {}".format(num_1, num_2, add))
+print("{} * {} = {}".format(num_1, num_2, multiply))
