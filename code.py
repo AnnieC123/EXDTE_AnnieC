a4a51ccc079598a92e733bd6e3c0f17958fe912e
AnnieC123 <anniechen@rhs.school.nz>
2025-07-15 10:29:28 +1200

    time.monotonic() code

--- code.py
+++ code.py
@@ -1,13 +1,6 @@
 import board
 import digitalio
 import time
-from adafruit_debouncer import Debouncer
-
-# Setting up the button
-button_pin = digitalio.DigitalInOut(board.GP12)
-button_pin.direction = digitalio.Direction.INPUT
-button_pin.pull = digitalio.Pull.UP
-my_button = Debouncer(button_pin)
 
 # Setting up the LEDs
 greenled = digitalio.DigitalInOut(board.GP17)
@@ -15,23 +8,11 @@ greenled.direction = digitalio.Direction.OUTPUT
 redled = digitalio.DigitalInOut(board.GP16)
 redled.direction = digitalio.Direction.OUTPUT
 
-while True:
-    my_button.update()
-    if my_button.value is True:
-        print("not pressed")
-        greenled.value = False
-        redled.value = False
-    if my_button.value is False:
-        print("pressed")
-        greenled.value = True
-        redled.value = True
-    if my_button.fell is True:
-        print("Just pressed")
-        greenled.value = True
-        redled.value = False
-    if my_button.rose is True:
-        print("Just released")
-        greenled.value = False
-        redled.value = True
-    time.sleep(0.5)
+start_time = time.monotonic()
 
+while start_time + 3 > time.monotonic():
+    greenled.value = True
+    time.sleep(0.1)
+    greenled.value = False
+    time.sleep(0.1)
+redled.value = True
